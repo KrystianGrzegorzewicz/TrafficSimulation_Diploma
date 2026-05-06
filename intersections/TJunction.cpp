f26b6239c31444783e42e0120b99ef1cc20344b1
KrystianGrzegorzewicz <kgrzeg@student.agh.edu.pl>
2026-05-06 14:04:45 +0200

    dodano prototyp ronda

--- intersections/TJunction.cpp
+++ intersections/TJunction.cpp
@@ -6,6 +6,8 @@ TJunction::TJunction() {
     std::vector<Vec2> travelPoints1;
     std::vector<Vec2> travelPoints2;
     std::vector<Vec2> travelPoints3;
+
+
     //z prawej do lewej
         travelPoints1 = {
             Vec2(100.0f, -1.75f),
@@ -79,11 +81,15 @@ TJunction::TJunction(int index) {
     std::vector<Vec2> travelPoints1;
     std::vector<Vec2> travelPoints2;
     std::vector<Vec2> travelPoints3;
+    std::vector<Vec2> travelPoints4;
+    std::vector<Vec2> travelPoints5;
+    std::vector<Vec2> travelPoints6;
+
     switch (index) {
         case 0:
             // Default T-junction
             break;
-        case 1:
+        case 1: //bezkonfliktowy prawoskret
             blocks.push_back(Block(-100.0f, -4.0f, 100.0f, -3.5f));
             blocks.push_back(Block(-100.0f, 3.5f, -3.5f, 4.0f));
             blocks.push_back(Block(3.5f, 3.5f, 100.0f, 4.0f));
@@ -140,7 +146,7 @@ TJunction::TJunction(int index) {
             };
             travels.push_back(Travel(travelPoints3, 1, 3));
 			break;
-        case 2:
+        case 2: //lewoskret jeden konflikt
 			//gorny z prawej do lewej
             blocks.push_back(Block(3.5f, 0.0f, 4.0f, 3.5f));
             blocks.push_back(Block(3.5f, 0.0f, 100.0f, 0.2f));
@@ -218,7 +224,7 @@ TJunction::TJunction(int index) {
             };
             travels.push_back(Travel(travelPoints3, 1, 3));
 			break;
-        case 3:
+		case 3: //pojedynczy pas, przejscie dla pieszych
             blocks.push_back(Block(-100.0f, -2.0f, 100.0f, -1.75f));
             blocks.push_back(Block(-100.0f, 1.75f, 100.0f, 2.0f));
             blocks.push_back(Block(0.0f, -1.75f, 5.0f, 1.75f, 10.0f, 5.0f));
@@ -235,6 +241,33 @@ TJunction::TJunction(int index) {
                 Vec2(100.0f, 0.0f)
             };
             travels.push_back(Travel(travelPoints1, 1, 1));
+        case 4: //proste rondo
+
+            //z gory w dol
+            travelPoints1 = {
+                Vec2(-100.0f, 0.0f),
+            };
+            //z gory w prawo
+            travelPoints2 = {
+                Vec2(-100.0f, 0.0f),
+            };
+            //z prawej w gore
+            travelPoints3 = {
+                Vec2(-100.0f, 0.0f),
+            };
+			//z prawej w dol
+            travelPoints4 = {
+                Vec2(-100.0f, 0.0f),
+            };
+			//z dolu w prawo
+            travelPoints5 = {
+                Vec2(-100.0f, 0.0f),
+            };
+			//z dolu w gore
+            travelPoints6 = {
+                Vec2(-100.0f, 0.0f),
+            };
+			travels.push_back(Travel(travelPoints1, 1, 1));
         default:
             std::cerr << "Unknown TJunction index: " << index << std::endl;
             break;
